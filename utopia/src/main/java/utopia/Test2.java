2fc3a68ff95ac723383377fcea1a76cc3dfde2e7
崔杨 <hoppo@163.com>
2019-01-18 17:30:14 +0800

    test2

--- utopia/src/main/java/utopia/Test2.java
+++ utopia/src/main/java/utopia/Test2.java
@@ -1,8 +1,7 @@
 package utopia;
 
-public class TestClass {
+public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("Utopia");
-		
+		System.out.println("kkk");
 	}
 }
