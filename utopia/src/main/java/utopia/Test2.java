2c08fa04ab85f447476e068a32c73f8e7fbbc53c
崔杨 <hoppo@163.com>
2019-01-18 17:40:54 +0800

    Revert "test2"
    This reverts commit 2fc3a68ff95ac723383377fcea1a76cc3dfde2e7.

--- utopia/src/main/java/utopia/Test2.java
+++
@@ -1,7 +0,0 @@
-package utopia;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println("kkk");
-	}
-}
--- utopia/src/main/java/utopia/TestClass.java
+++ utopia/src/main/java/utopia/TestClass.java
@@ -3,6 +3,5 @@ package utopia;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Utopia");
-		
 	}
 }
