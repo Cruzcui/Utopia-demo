dcb6a8546618269f2374b73de00a460f038c3b0a
崔杨idea <hoppo@163.com>
2019-01-18 22:22:44 +0800

    这里是测试idea

--- utopia/src/main/java/utopia/TestClass.java
+++ utopia/src/main/java/utopia/TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Utopia");
 		System.out.printf("这里是idea添加");
-		System.out.printf("这里是测试");
+		System.out.printf("这里是测试");//hahahaha
 	}
 }
