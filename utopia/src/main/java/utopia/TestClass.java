dd378a9349db4ae8892a35fd852407f7dc28e5f5
崔杨 <hoppo@163.com>
2019-01-25 15:43:02 +0800

    配置文件添加

--- utopia/src/main/java/utopia/TestClass.java
+++ utopia/src/main/java/utopia/TestClass.java
@@ -3,7 +3,5 @@ package utopia;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Utopia");
-		System.out.println("123");
-		System.out.println("456");
 	}
 }
