3373463599e130e7d054a2cccb1cc7f9d70641ba
崔杨 <hoppo@163.com>
2019-01-25 12:10:53 +0800

    修改提交

--- utopia/src/main/java/utopia/TestClass.java
+++ utopia/src/main/java/utopia/TestClass.java
@@ -3,7 +3,6 @@ package utopia;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Utopia");
-		System.out.printf("这里是idea添加");
-		System.out.printf("这里是测试");//hahahaha
+
 	}
 }
